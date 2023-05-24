f3e74f383a0046c372452f261e1291114de1b166
Hadi Ahmadi <thehadiahmadi@gmail.com>
2023-05-24 23:26:51 +0430

    Try to publish

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ export function Button (props) {
     console.log('Button', {props})
     return `<button>TEST Button</button>`
 } 
+export function Card (props) {
+    console.log('Card', {props})
+    return `<div>TEST Card</div>`
+} 
