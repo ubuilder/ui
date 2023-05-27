b59836eadd9750e0e0c5ede516cb086a94a297c3
Hadi Ahmadi <thehadiahmadi@gmail.com>
2023-05-27 18:59:17 +0430

    update dependency

--- serve.js
+++ serve.js
@@ -2,13 +2,12 @@ import express from 'express'
 
 const app = express()
 
-
-
 app.use('/components/*', express.static('./build'))
 
 app.get('/', (req, res) => {
     res.redirect('/components')
 })
+
 app.listen(3000, () => {
     console.log('listening on port '+ 3000)
 })
