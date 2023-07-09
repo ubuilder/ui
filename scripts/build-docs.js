30e5bf19d4a6ff69c875283105713318ab470e92
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-07-09 14:22:58 +0430

    rename package to ui

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,45 +1,42 @@
 import {
-    cpSync,
-    existsSync,
-    readFileSync,
-    mkdirSync,
-    readdirSync,
-    rmSync,
-    writeFileSync,
-  } from "fs";
-  import { renderTemplate } from "@ulibs/router";
+  cpSync,
+  existsSync,
+  mkdirSync,
+  readdirSync,
+  rmSync,
+  writeFileSync,
+} from "fs";
+import { renderTemplate } from "@ulibs/router";
 
-  
-  const files = readdirSync("./src/docs/pages");
-  
-  if (!existsSync("./build/components")) {
-    mkdirSync("./build/components", {recursive: true});
-  } else {
-    rmSync("./build/components", { recursive: true });
-    mkdirSync("./build/components");
-  }
-    
-  for (let file of files) {
-    if (file.endsWith(".js")) {
-      import("./src/docs/pages/" + file).then((module) => {
-        const page = renderTemplate(module.default({prefix: '/components/'}));
-  
-        if (file == "index.js") {
-          writeFileSync("./build/components/" + "index.html", page);
-        } else {
-          if (!existsSync("./build/components/" + file.replace(".js", ""))) {
-            mkdirSync("./build/components/" + file.replace(".js", ""));
-          }
-  
-          writeFileSync(
-            "./build/components/" + file.replace(".js", "") + "/index.html",
-            page
-          );
+const files = readdirSync("./src/docs/pages");
+
+if (!existsSync("./build/ui")) {
+  mkdirSync("./build/ui", { recursive: true });
+} else {
+  rmSync("./build/ui", { recursive: true });
+  mkdirSync("./build/ui");
+}
+
+for (let file of files) {
+  if (file.endsWith(".js")) {
+    import("./src/docs/pages/" + file).then((module) => {
+      const page = renderTemplate(module.default({ prefix: "/components/" }));
+
+      if (file == "index.js") {
+        writeFileSync("./build/ui/" + "index.html", page);
+      } else {
+        if (!existsSync("./build/ui/" + file.replace(".js", ""))) {
+          mkdirSync("./build/ui/" + file.replace(".js", ""));
         }
-      });
-    }
+
+        writeFileSync(
+          "./build/ui/" + file.replace(".js", "") + "/index.html",
+          page
+        );
+      }
+    });
   }
-  
-  cpSync("./dist/ulibs.js", "./build/components/ulibs.js");
-  cpSync("./dist/styles.css", "./build/components/styles.css");
-  
+}
+
+cpSync("./dist/ulibs.js", "./build/ui/ulibs.js");
+cpSync("./dist/styles.css", "./build/ui/styles.css");
