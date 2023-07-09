537e8089ddaad955b37a3ab9c1a841e4c75f8dbd
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-07-09 14:24:20 +0430

    update

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -8,7 +8,6 @@ import * as sass from "sass";
 
 
 const css = sass.compile("./src/styles/index.scss");
-writeFileSync("./build/styles.css", css.css);
 
 if (!existsSync("./dist")) {
   mkdirSync("./dist");
