f349490a30d7055360e1e5a0da1925671fbc57ea
Jawad <jawad.azizi1398@gmail.com>
2023-07-21 18:47:36 +0430

    organized abit

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -1,3 +1,91 @@
+import { Base } from "../utils.js";
+import { View } from "./View.js";
+
+/**
+* @type {import('.').Tooltip}
+*/
+export const Tooltip = Base({
+  render($props, $slots){
+    const {
+      component = "tooltip",
+      size = "md",
+      arrow = true,
+      offset,
+      margin,
+      placement = 'bottom',
+      arrowMargin,
+      trigger = "hover",//click, hover
+      ...restProps
+    } = $props;
+
+    const props = {
+      ...restProps,
+      component,
+      cssProps: {
+        size,
+      },
+    };
+
+    if(offset)      props["u-tooltip-offset"] = offset
+    if(placement)   props["u-tooltip-placement"] = placement
+    if(margin)      props["u-tooltip-margin"] = margin
+    if(arrowMargin) props["u-tooltip-arrow-margin"] = arrowMargin
+    if(trigger)     props["u-tooltip-trigger"] = trigger
+
+    
+    let content =  View(props,  $slots)
+    if(arrow)content =  View(props, [$slots, TooltipArrow([View({tag: 'div', "u-tooltip-arrow-inside": ''})])])
+    
+    return content
+  }
+});
+
+/**
+* @type {import('.').TooltipArrow}
+*/
+export const TooltipArrow = Base({
+  render($props, $slots){
+    const {
+      component = "tooltip-arrow",
+      size = "md",
+      ...restProps
+    } = $props;
+
+    const props = {
+      ...restProps,
+      component,
+      cssProps: {
+        size,
+      },
+    };
+
+    return View(props, $slots);
+  }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//previous usage
+//
+// 
 // import { Base } from "../utils.js";
 // import { View } from "./View.js";
 
@@ -106,67 +194,3 @@
 
 
 
-import { Base } from "../utils.js";
-import { View } from "./View.js";
-
-/**
-* @type {import('.').Tooltip}
-*/
-export const Tooltip = Base({
-  render($props, $slots){
-    const {
-      component = "tooltip",
-      size = "md",
-      arrow = true,
-      offset,
-      margin,
-      placement = 'bottom',
-      arrowMargin,
-      trigger = "hover",//click, hover
-      ...restProps
-    } = $props;
-
-    const props = {
-      ...restProps,
-      component,
-      cssProps: {
-        size,
-      },
-    };
-
-    if(offset)      props["u-tooltip-offset"] = offset
-    if(placement)   props["u-tooltip-placement"] = placement
-    if(margin)      props["u-tooltip-margin"] = margin
-    if(arrowMargin) props["u-tooltip-arrow-margin"] = arrowMargin
-    if(trigger)     props["u-tooltip-trigger"] = trigger
-
-    
-    let content =  View(props,  $slots)
-    if(arrow)content =  View(props, [$slots, TooltipArrow([View({tag: 'div', "u-tooltip-arrow-inside": ''})])])
-    
-    return content
-  }
-});
-
-/**
-* @type {import('.').TooltipArrow}
-*/
-export const TooltipArrow = Base({
-  render($props, $slots){
-    const {
-      component = "tooltip-arrow",
-      size = "md",
-      ...restProps
-    } = $props;
-
-    const props = {
-      ...restProps,
-      component,
-      cssProps: {
-        size,
-      },
-    };
-
-    return View(props, $slots);
-  }
-});
