218d5cb596c18a318e111a31169854c70ad3e471
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-07-22 15:42:13 +0430

    add initial value for input

--- src/script/input.js
+++ src/script/input.js
@@ -2,6 +2,10 @@ export function Input(Alpine) {
   
     Alpine.directive("input", (el) => {
       Alpine.bind(el, {
+        // initial value
+        'u-init'() {
+          this.$data[el.getAttribute('name')] = el.value
+        },
         "u-on:input"(e) {
           this.$data[el.getAttribute("name")] = e.target.value;
         },
