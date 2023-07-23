fb7d86df73cf7b4fc884e09257ab0678a89df60a
Jawad <jawad.azizi1398@gmail.com>
2023-07-24 01:54:20 +0430

    some changes

--- src/docs/pages/popover.js
+++ src/docs/pages/popover.js
@@ -1,152 +1,357 @@
-import { Button } from "../../components/Button.js";
 import { DocPage } from "../components/DocPage.js";
 import { Section } from "../components/Section.js";
-import { Popover} from "../../components/index.js";
-import { Preview } from '../components/Preview.js'
+import { Preview } from "../components/Preview.js";
+import {
+  Popover,
+  View,
+  Container,
+  Form,
+  Input,
+  Col,
+  Button,
+  Row,
+} from "../../components/index.js";
 
 export default function () {
-  return DocPage({ name: "Tabs" }, [
+  return DocPage({ name: "Popover" }, [
     Section(
       {
-        title: "popover",
+        title: "default",
         description: "the default values acts like this",
       },
       [
         Preview({
-          code: `Button(["click me", Popover("Hi, it is popover")])]`,
+          code: `Button(["click me", Popover("Hi, it is popover")])`,
         }),
       ]
     ),
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "trigger",
+        description: "default value is 'click', valuse is 'click' and 'hover' ",
       },
       [
         Preview({
-          code: `Button(["hover me", Popover({trigger: 'hover'},"Hi, it is popover")])]`,
+          code: `Button(["hover me", Popover({trigger: 'hover'},"Hi, it is popover")])`,
         }),
       ]
     ),
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "persistant mode",
+        description:
+          "close or not close popover when hover over it, default value is 'true'",
       },
       [
         Preview({
-          code: `Button(["hover, none-persistant", Popover({trigger: 'hover',persistant: 'false' },"Hi, it is popover")])]`,
+          code: `Button(["hover, none-persistant", Popover({trigger: 'hover',persistant: 'false' },"Hi, it is popover")])`,
         }),
       ]
     ),
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "arrow",
+        description: "default is value is false",
       },
       [
         Preview({
-          code: `Button(["hover, none-persistant, arrow", Popover({trigger: 'hover',persistant: 'false', arrow: 'true' },"Hi, it is popover")])]`,
+          code: `Button(["hover, none-persistant, arrow", Popover({trigger: 'hover',persistant: 'false', arrow: 'true' },"Hi, it is popover")])`,
         }),
       ]
     ),
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "placement",
+        description:
+          "default is 'bottom', values can be any of  top, bottom, ..., top-start, top-end,...",
       },
       [
         Preview({
           code: `
-          Button(["hover, persistant, arrow, top", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'top' },"Hi, it is popover")])],
-          Button(["hover, persistant, arrow, left", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'left' },"Hi, it is popover")])],
-          Button(["hover, persistant, arrow, bottom", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'bottom' },"Hi, it is popover")])],
-          Button(["hover, persistant, arrow, right", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'right' },"Hi, it is popover")])],
+          Button(["hover, persistant, arrow, top", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'top' },"Hi, it is popover")]),
+          Button(["hover, persistant, arrow, left", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'left' },"Hi, it is popover")]),
+          Button(["hover, persistant, arrow, bottom", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'bottom' },"Hi, it is popover")]),
+          Button(["hover, persistant, arrow, right", Popover({trigger: 'hover',persistant: 'true', arrow: 'true', placement: 'right' },"Hi, it is popover")]),
           `,
         }),
       ]
     ),
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "component inside",
+        description: "components inside popover",
       },
       [
         Preview({
           code: `
-          Button(["hover, persistant", Popover({trigger: 'hover',persistant: 'true',},[
-            Button(["hover, persistant", Popover({trigger: 'hover',persistant: 'true',},'popover')]),
-            Button(["hover, none-persistant", Popover({trigger: 'hover',persistant: 'false',},'popover')]),
-          ])])],
+          Button([
+            "hover, persistant",
+            Popover({ trigger: "hover", persistant: "true" }, [
+              View([
+                View("this is view1"),
+                Popover({ trigger: "hover", persistant: "true" }, "popover"),
+              ]),
+              View([
+                View("this is view2"),
+                Popover({ trigger: "hover", persistant: "true" }, "popover"),
+              ]),
+            ]),
+          ]),
+          `,
+        }),
+      ]
+    ),
+    Section(
+      {
+        title: "focusable",
+        description:
+          "defaultvalue is true, in case of need you can make it false ",
+      },
+      [
+        Preview({
+          code: `
+          Button([
+            "tooltip only",
+            Popover(
+              { trigger: "hover", focusAble: false, arrow: true },
+              Input({ name: "test" })
+            ),
+          ]),
+          `,
+        }),
+      ]
+    ),
+    Section(
+      {
+        title: "component inside",
+        description: "components inside popover",
+      },
+      [
+        Preview({
+          code: `
+          View({ border: "sm", style: "width: max-content" }, [
+            "hover, persistant",
+            Popover({ trigger: "hover", persistant: "true" }, [
+              Container({ size: "xl", mx: "auto" }, [
+                View({ tag: "h3" }, "Login form"),
+  
+                Form([
+                  Col([
+                    Row([
+                      View({ tag: "div", style: "display: block" }, "Username"),
+                      Input({ name: "username" }),
+                    ]),
+                    Row([View("password"), Input({ name: "password"})]),
+  
+                    Row({ border: true, p: "sm", mb: "sm" }, [
+                      View([
+                        View([
+                          "Username: ",
+                          View({ tag: "span", "u-text": "username" }),
+                        ]),
+                        View([
+                          "password: ",
+                          View({ tag: "span", "u-text": "password" }),
+                        ]),
+                      ]),
+                    ]),
+  
+                    Button({ type: "submit" ,color: 'primary'}, "Submit"),
+                  ]),
+                ]),
+              ]),
+            ]),
+          ]),
           `,
         }),
       ]
     ),
 
+    //in development mode the Preview does not support
+    View(
+      { pt: "lg", pb: "lg", style: "color: red" },
+      "&lt;=============-- with out preview --===========&gt"
+    ),
+    Section(
+      {
+        title: "default",
+        description: "the default values acts like this",
+      },
+      [Button(["click me", Popover("Hi, it is popover")])]
+    ),
+    Section(
+      {
+        title: "trigger",
+        description: "default value is 'click', valuse is 'click' and 'hover' ",
+      },
+      [Button(["hover me", Popover({ trigger: "hover" }, "Hi, it is popover")])]
+    ),
+    Section(
+      {
+        title: "persistant mode",
+        description:
+          "close or not close popover when hover over it, default value is 'true'",
+      },
+      [
+        Button([
+          "hover, none-persistant",
+          Popover(
+            { trigger: "hover", persistant: "false" },
+            "Hi, it is popover"
+          ),
+        ]),
+      ]
+    ),
+    Section(
+      {
+        title: "arrow",
+        description: "default is value is false",
+      },
+      [
+        Button([
+          "hover, none-persistant, arrow",
+          Popover(
+            { trigger: "hover", persistant: "false", arrow: "true" },
+            "Hi, it is popover"
+          ),
+        ]),
+      ]
+    ),
+    Section(
+      {
+        title: "placement",
+        description:
+          "default is 'bottom', values can be any of  top, bottom, ..., top-start, top-end,...",
+      },
+      [
+        Button([
+          "hover, persistant, arrow, top",
+          Popover(
+            {
+              trigger: "hover",
+              persistant: "true",
+              arrow: "true",
+              placement: "top",
+            },
+            "Hi, it is popover"
+          ),
+        ]),
+        Button([
+          "hover, persistant, arrow, left",
+          Popover(
+            {
+              trigger: "hover",
+              persistant: "true",
+              arrow: "true",
+              placement: "left",
+            },
+            "Hi, it is popover"
+          ),
+        ]),
+        Button([
+          "hover, persistant, arrow, bottom",
+          Popover(
+            {
+              trigger: "hover",
+              persistant: "true",
+              arrow: "true",
+              placement: "bottom",
+            },
+            "Hi, it is popover"
+          ),
+        ]),
+        Button([
+          "hover, persistant, arrow, right",
+          Popover(
+            {
+              trigger: "hover",
+              persistant: "true",
+              arrow: "true",
+              placement: "right",
+            },
+            "Hi, it is popover"
+          ),
+        ]),
+      ]
+    ),
 
+    Section(
+      {
+        title: "component inside",
+        description: "components inside popover",
+      },
+      [
+        Button([
+          "hover, persistant",
+          Popover({ trigger: "hover", persistant: "true" }, [
+            View([
+              View("this is view1"),
+              Popover({ trigger: "hover", persistant: "true" }, "popover"),
+            ]),
+            View([
+              View("this is view2"),
+              Popover({ trigger: "hover", persistant: "true" }, "popover"),
+            ]),
+          ]),
+        ]),
+      ]
+    ),
+    Section(
+      {
+        title: "focusable",
+        description:
+          "defaultvalue is true, in case of need you can make it false ",
+      },
+      [
+        Button([
+          "tooltip only",
+          Popover(
+            { trigger: "hover", focusAble: false, arrow: true },
+            Input({ name: "test" })
+          ),
+        ]),
+      ]
+    ),
 
     Section(
       {
-        title: "popover",
-        description: "the default active tabs is tabs one if not specified",
+        title: "form inside",
       },
-      [Button(["arrow, click", Popover({arrow: true, trigger: 'click'},"popover")])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["focusable, hover", Popover({trigger: 'hover', focusAble: true},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["focusable, hover, arrow", Popover({trigger: 'hover', arrow: true, persistant: true},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["not persistant, hover, arrow", Popover({trigger: 'hover', arrow: true, persistant: false},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["focusable, click, arrow", Popover({trigger: 'click', arrow: true, focusAble: true},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["i am popover", Popover({arrow: true},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["placement left", Popover({arrow: 'true', placement: 'left'},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["placemenet right", Popover({arrow: true, placement: 'right'},["popover"])])]
-    ),
-    Section(
-      {
-        title: "tabs",
-        description: "the default active tabs is tabs one if not specified",
-      },
-      [Button(["placemnt top", Popover({arrow: true, placement: 'top'},["popover"])])]
+      [
+        View({ border: "sm", style: "width: max-content" }, [
+          "hover, persistant",
+          Popover({ trigger: "hover", persistant: "true" }, [
+            Container({ size: "xl", mx: "auto" }, [
+              View({ tag: "h3" }, "Login form"),
+
+              Form([
+                Col([
+                  Row([
+                    View({ tag: "div", style: "display: block" }, "Username"),
+                    Input({ name: "username" }),
+                  ]),
+                  Row([View("password"), Input({ name: "password" })]),
+
+                  Row({ border: true, p: "sm", mb: "sm" }, [
+                    View([
+                      View([
+                        "Username: ",
+                        View({ tag: "span", "u-text": "username" }),
+                      ]),
+                      View([
+                        "password: ",
+                        View({ tag: "span", "u-text": "password" }),
+                      ]),
+                    ]),
+                  ]),
+
+                  Button({ type: "submit", color: "primary" }, "Submit"),
+                ]),
+              ]),
+            ]),
+          ]),
+        ]),
+      ]
     ),
   ]);
 }
