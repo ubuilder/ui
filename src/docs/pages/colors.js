e2cc29805377a00275b494b816f3529d6ed8e368
TheHadiAhmadi <thehadiahmadi@gmail.com>
2023-07-25 22:28:39 +0430

    update colors

---
+++ src/docs/pages/colors.js
@@ -0,0 +1,28 @@
+import { Row, Col, For, View, } from "../../components/index.js";
+import { DocPage } from "../components/DocPage.js";
+import { Section } from "../components/Section.js";
+
+let colorNames = [
+  "primary",
+  "secondary",
+  "success",
+  "info",
+  "warning",
+  "error",
+  'light',
+  'dark'
+];
+let colorValues = [100, 200, 300, 400, 500, 600, 700, "content"];
+
+export default () => {
+  return DocPage({ name: "Color System" }, [
+    Section({ title: "colors" }, [
+      colorNames.map((name) =>
+        Row({mb: 'xxs'}, [
+          Col({ col: 3, py: "xs", px: "md", bgColor: name, textColor: 'light' }, name),
+          colorValues.map((value) => Col({ col: 1, py: "xs", px: "md", style: `background-color: var(--color-${name}-${value}); color: black` }, value)),
+        ])
+      ),
+    ]),
+  ]);
+};
--- src/docs/pages/index.js
+++ src/docs/pages/index.js
@@ -1,60 +1,108 @@
 import { Col, Container, Row } from "../../components/GridSystem.js";
 import { View } from "../../components/View.js";
-import { Card, CardBody } from "../../components/index.js";
+import { Badge, Card, CardBody } from "../../components/index.js";
 
-
-
-
+const components = {
+  accordion: { title: "Accordion", new: true },
+  alert: { title: "Alert" },
+  autocomplete: { title: "Autocomplete" },
+  avatar: { title: "Avatar" },
+  badge: { title: 'Badge',new: true },
+  breadcrumb: { title: "Breadcrumb" },
+  button: { title: "Button" },
+  card: { title: "Card" },
+  checkbox: { title: "Checkbox & Radio" },
+  codeeditor: {title: 'Code Editor', new: true},
+  divider: { title: "Divider" },
+  dropdown: { title: "Dropdown" },
+  form: {title: "Form", new: true },
+  formfield: { title: "FormField" , new: true },
+  grid: { title: "Grid" },
+  icon: { title: "Icon" },
+  image: {title: 'Image', new: true },
+  input: {title: 'Input', new: true },
+  modal: {title: 'Modal'},
+  popover: { title: "Popover" },
+  progress: { title: "Progress" },
+  radio: {title: "Radio", new: true },
+  select: {title: 'Select', new: true },
+  spinner: { title: "Spinner" },
+  switch: {title: 'Switch'},
+  table: {title: 'Table'},
+  tabs: { title: "Tabs" },
+  textarea: {title: 'Textarea', new: true },
+  tooltip: { title: "Tooltip" },
+  view: { title: "View" },
+};
 
 export default ({ prefix }) => {
-  function Item({ slug, text }) {
-    return Col({ col: 12, colSm: 6, colLg: 4 }, [
-      
-      Card({ tag: 'a', d: 'block', href: `${prefix}${slug}`, style: 'text-decoration: none; color: var(--color-base-900)' }, [
-        CardBody(text)
-      ]),
-    ]);
+  function ComponentItems() {
+    return Object.keys(components).map((key) =>
+      Item({ slug: key, text: components[key].title, tags: components[key].new ? ['New'] : [] })
+    );
   }
 
-  return Container({ size: "xl", mx: "auto", my: "xl" }, [
-    // tag("a", { href: "/" }, "UBuilder"),
-    View({ tag: "h1" }, "Components"),
-    View({ tag: "h3", mb: "lg", mt: 'xxs', style: 'color: var(--color-base-800); font-weight: 400' }, "UI Components for NodeJS"),
-
-    View({mb: 'md', mt: 'sm', style: 'line-height: var(--size-lg)'}, 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi at numquam est ad unde. Quibusdam blanditiis tempora unde! Eum quam ex totam autem obcaecati fuga quidem dignissimos laudantium et? Nihil, consequatur voluptates reiciendis pariatur tenetur architecto cumque doloremque quas incidunt facilis exercitationem esse deleniti totam dolores dicta commodi suscipit eius.'),
-
-    Row([
-      Item({ slug: 'installation', text: 'Installation'}),
-      Item({ slug: 'basics', text: 'Basics'}),
-
-      Col({w: 100}, View({tag: 'h3', pt: 'sm', style: 'font-weight: normal'}, 'Components')),
-      Item({ slug: "autocomplete", text: "Autocomplete" }),
-      Item({ slug: "avatar", text: "Avatar" }),      
-      Item({ slug: "alert", text: "Alert" }),      
-      Item({ slug: "breadcrumb", text: "Breadcrumb" }),
-      Item({ slug: "button", text: "Button" }),
-      Item({ slug: "card", text: "Card" }),
-      Item({ slug: "checkbox", text: "Checkbox & Radio" }),
-      Item({ slug: "divider", text: "Divider" }),
-      Item({ slug: "dropdown", text: "Dropdown" }),
-      Item({ slug: "form-field", text: "FormField" }),
-      Item({ slug: "grid", text: "Grid" }),
-      Item({ slug: "icon", text: "Icon" }),
-      Item({ slug: "progress", text: "Progress" }),
-      Item({ slug: "spinner", text: "Spinner" }),
-      Item({ slug: "tabs", text: "Tabs" }),
-      Item({ slug: "tooltip", text: "Tooltip" }),
-      Item({ slug: "popover", text: "Popover" }),
-
-      Item({ slug: "view", text: "View" }),
-
-      Col({w: 100}, View({tag: 'h3', pt: 'sm', style: 'font-weight: normal'}, 'Examples')),
+  function ExampleItems() {
+    return [
       Item({ slug: "accordion-example", text: "Accordion Example" }),
       Item({ slug: "card-example", text: "Card Example" }),
       Item({ slug: "dynamic-form", text: "Form" }),
       Item({ slug: "login", text: "Login" }),
       Item({ slug: "signup", text: "Signup" }),
       Item({ slug: "table", text: "Table & Modal" }),
+    ];
+  }
+
+  function Heading({ title }) {
+    return Col(
+      { w: 100 },
+      View({ tag: "h3", pt: "sm", style: "font-weight: normal" }, title)
+    );
+  }
+
+  function Item({ slug, text, tags = [] }) {
+    return Col({ col: 12, colSm: 6, colLg: 4 }, [
+      Card(
+        {
+          tag: "a",
+          d: "block",
+          href: `${prefix}${slug}`,
+          style: "text-decoration: none; color: var(--color-base-900)",
+        },
+        [CardBody([View({d: 'flex', align: 'center', gap: 'xs'}, [text, tags.map(tag => Badge({color: 'success'}, tag))])])]
+      ),
+    ]);
+  }
+
+  return Container({ size: "xl", mx: "auto", my: "xl" }, [
+    // tag("a", { href: "/" }, "UBuilder"),
+    View({ tag: "h1" }, "Components"),
+    View(
+      {
+        tag: "h3",
+        mb: "lg",
+        mt: "xxs",
+        style: "color: var(--color-base-800); font-weight: 400",
+      },
+      "UI Components for NodeJS"
+    ),
+
+    View(
+      { mb: "md", mt: "sm", style: "line-height: var(--size-lg)" },
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eligendi at numquam est ad unde. Quibusdam blanditiis tempora unde! Eum quam ex totam autem obcaecati fuga quidem dignissimos laudantium et? Nihil, consequatur voluptates reiciendis pariatur tenetur architecto cumque doloremque quas incidunt facilis exercitationem esse deleniti totam dolores dicta commodi suscipit eius."
+    ),
+
+    Row([
+      Item({ slug: "installation", text: "Installation" }),
+      Item({ slug: "basics", text: "Basics" }),
+      Item({ slug: "colors", text: "Colors" }),
+
+      Heading({ title: "Components" }),
+
+      ComponentItems(),
+
+      Heading({ title: "Examples" }),
+      ExampleItems(),
     ]),
   ]);
 };
